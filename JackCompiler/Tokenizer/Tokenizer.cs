cfd208485fd4fb2fae86e4f7ba7507c135dccd25
Gary Lee <leechisan2007@hotmail.com>
2023-07-23 13:52:42 +0800

    Init

---
+++ JackCompiler/Tokenizer/Tokenizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackCompiler.Tokenizer;
+
+public class Tokenizer
+{
+    public static Tokenizer FromSourceFile(string sourceFile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Tokenizer FromSourceCode(string sourceCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Advance()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool HasMoreTokens()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ JackCompilerTests/Tokenizer/TokenizerTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using JackCompiler.Tokenizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackCompiler.Tokenizer.Tests;
+
+public class TokenizerTests
+{
+    [Fact]
+    public void FromSourceFileTest()
+    {
+        Assert.True(false, "This test needs an implementation");
+    }
+}
