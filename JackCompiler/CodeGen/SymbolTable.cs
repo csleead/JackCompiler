df53bf636ca8cdf995fea530d22f15cf6de8992d
Gary Lee <leechisan2007@hotmail.com>
2023-09-07 22:26:54 +0800

    Implement symbol table

---
+++ JackCompiler/CodeGen/SymbolTable.cs
@@ -0,0 +1,58 @@
+﻿namespace JackCompiler;
+
+public class SymbolTable
+{
+    private readonly Dictionary<string, IdentifierInfo> _identifiers = new();
+    private readonly Dictionary<IdentifierKind, int> _offsets = new();
+
+    private readonly SymbolTable? _classLevelTable;
+    private readonly string _className;
+
+    public SymbolTable(string className)
+    {
+        _className = className;
+    }
+
+    private SymbolTable(SymbolTable classLevelTable)
+    {
+        _className = classLevelTable._className;
+        _classLevelTable = classLevelTable;
+    }
+
+    public SymbolTable StartSubroutine()
+    {
+        var table = new SymbolTable(this);
+        table.AddIdentifier("this", _className, IdentifierKind.Arg);
+        return table;
+    }
+
+    public void AddIdentifier(string name, string type, IdentifierKind kind)
+    {
+        if (!_offsets.ContainsKey(kind))
+        {
+            _offsets.Add(kind, 0);
+        }
+
+        _identifiers.Add(name, new IdentifierInfo(type, kind, _offsets[kind]));
+        _offsets[kind]++;
+    }
+
+    public IdentifierInfo GetIdentifier(string name)
+    {
+        return _identifiers.GetValueOrDefault(name)
+            ?? _classLevelTable?.GetIdentifier(name)
+            ?? throw new Exception($"Identifier {name} not found");
+    }
+}
+
+public enum IdentifierKind
+{
+    Static,
+    Field,
+    Arg,
+    Var,
+}
+
+public record class IdentifierInfo(string Type, IdentifierKind Kind, int Offset)
+{
+}
---
+++ JackCompilerTests/CodeGen/SymbolTableTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Argon;
+using JackCompiler;
+
+namespace JackCompilerTests;
+
+[UsesVerify]
+public class SymbolTableTest
+{
+    [Fact]
+    public async Task TestClassLevelSymbolTable()
+    {
+        var classLevelTable = new SymbolTable("DummyClass");
+        classLevelTable.AddIdentifier("fieldX", "int", IdentifierKind.Field);
+        classLevelTable.AddIdentifier("fieldY", "string", IdentifierKind.Field);
+        classLevelTable.AddIdentifier("staticFieldX", "boolean", IdentifierKind.Static);
+        classLevelTable.AddIdentifier("staticFieldY", "string", IdentifierKind.Static);
+
+        var result = new[] { "fieldX", "fieldY", "staticFieldX", "staticFieldY" }
+            .Select(name => (name, classLevelTable.GetIdentifier(name)))
+            .ToDictionary(x => x.Item1, x => x.Item2);
+        await Verify(result)
+            .AddExtraSettings(x => x.DefaultValueHandling = DefaultValueHandling.Include);
+    }
+
+    [Fact]
+    public async Task TestSubroutineLevelTable()
+    {
+        var classLevelTable = new SymbolTable("DummyClass");
+        classLevelTable.AddIdentifier("fieldX", "int", IdentifierKind.Field);
+        classLevelTable.AddIdentifier("staticFieldX", "int", IdentifierKind.Static);
+
+        var subRoutineLevelTable = classLevelTable.StartSubroutine();
+        subRoutineLevelTable.AddIdentifier("localX", "int", IdentifierKind.Var);
+        subRoutineLevelTable.AddIdentifier("localY", "int", IdentifierKind.Var);
+        subRoutineLevelTable.AddIdentifier("arg0", "int", IdentifierKind.Arg);
+        subRoutineLevelTable.AddIdentifier("arg1", "int", IdentifierKind.Arg);
+
+        var result = new[] { "fieldX", "staticFieldX", "localX", "localY", "this", "arg0", "arg1" }
+            .Select(name => (name, subRoutineLevelTable.GetIdentifier(name)))
+            .ToDictionary(x => x.Item1, x => x.Item2);
+        await Verify(result)
+            .AddExtraSettings(x => x.DefaultValueHandling = DefaultValueHandling.Include);
+    }
+}
